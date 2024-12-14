03ea9bbe4175448b0b1c9375428bde18471548ae
Shu-YuanYang <zantgoron@gmail.com>
2024-12-14 22:26:33 +0000

    Enabled notification custom sound.

--- messaging/notification.js
+++ messaging/notification.js
@@ -7,7 +7,21 @@ const send_notification = async (messaging_client, message_title, message_body, 
 			body: message_body
 		},
 		data: extra_data,
-		token: registration_token
+		token: registration_token,
+		android: {
+			notification: {
+				defaultSound: true,
+				notificationCount: 1,
+				sound: "notification_ringtone.mp3",
+				channelId: "local_fcm_default_channel"
+			},
+			ttl: 20000
+		},
+		apns: {
+			payload: {
+				aps: { badge: 1, sound: "default" }
+			}
+		}
 	};
 
 	let response = await messaging_client.send(message_obj);
